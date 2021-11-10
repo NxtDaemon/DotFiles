5f65b873018b945f2ec117edde8f3128092b55fd
Luke Aaron Cotterell <NxtDaemon@NxtArch>
2021-11-10 16:01:38 +0000

    Fixed Pub-IP script and changed inteval to 60s

--- polybar/pub-ip.sh
+++ polybar/pub-ip.sh
@@ -4,7 +4,7 @@ IP=$(curl -4 -sf ifconfig.co --silent  --stderr - | grep -oE "([0-9]{1,3}\.){3}(
 echo "Executed" > /tmp/E
 
 if pgrep -x openvpn > /dev/null; then
-	    echo "  VPN:  $IP"
+	echo "  VPN: $(ifconfig tun0 | grep "inet " | awk '{print $2}')"
     else
 	        echo "" > /tmp/E & echo " $IP"
 fi
